30e0c066f78022a277f44cafdccc138819288d6d
gixousiyq <xbourgleh@gmail.com>
2024-06-17 03:08:45 +0300

    vayu: Initial display bringup
    Display and DSC seems to be working, Theres a white thingy that appears at the first moments of the boot then disapppers and display works
    UFS is suspected to be working too

--- drivers/gpu/drm/panel/panel-huaxing-nt36672c.c
+++ drivers/gpu/drm/panel/panel-huaxing-nt36672c.c
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// Copyright (c) 2023 Arseniy Velikanov <adomerlee@gmail.com>
+// Copyright (c) 2024 FIXME
+// Generated with linux-mdss-dsi-panel-driver-generator from vendor device tree:
+//   Copyright (c) 2013, The Linux Foundation. All rights reserved. (FIXME)
 
 #include <linux/delay.h>
 #include <linux/gpio/consumer.h>
@@ -9,35 +11,29 @@
 
 #include <video/mipi_display.h>
 
+#include <drm/display/drm_dsc.h>
+#include <drm/display/drm_dsc_helper.h>
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_modes.h>
 #include <drm/drm_panel.h>
-#include <drm/display/drm_dsc.h>
-#include <drm/display/drm_dsc_helper.h>
 
-struct nt36672c {
+
+struct j20s_42_02_0b_dsc {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
+	struct drm_dsc_config dsc;
+	struct regulator *supply;
 	struct gpio_desc *reset_gpio;
-	struct regulator_bulk_data supplies[3];
 	bool prepared;
 };
 
 static inline
-struct nt36672c *to_nt36672c(struct drm_panel *panel)
+struct j20s_42_02_0b_dsc *to_j20s_42_02_0b_dsc(struct drm_panel *panel)
 {
-	return container_of(panel, struct nt36672c, panel);
+	return container_of(panel, struct j20s_42_02_0b_dsc, panel);
 }
 
-#define dsi_dcs_write_seq(dsi, seq...) do {				\
-		static const u8 d[] = { seq };				\
-		int ret;						\
-		ret = mipi_dsi_dcs_write_buffer(dsi, d, ARRAY_SIZE(d));	\
-		if (ret < 0)						\
-			return ret;					\
-	} while (0)
-
-static void nt36672c_reset(struct nt36672c *ctx)
+static void j20s_42_02_0b_dsc_reset(struct j20s_42_02_0b_dsc *ctx)
 {
 	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
 	usleep_range(5000, 6000);
@@ -47,117 +43,120 @@ static void nt36672c_reset(struct nt36672c *ctx)
 	usleep_range(10000, 11000);
 }
 
-static int nt36672c_on(struct nt36672c *ctx)
+static int j20s_42_02_0b_dsc_on(struct j20s_42_02_0b_dsc *ctx)
 {
 	struct mipi_dsi_device *dsi = ctx->dsi;
 	struct device *dev = &dsi->dev;
 	int ret;
 
-	dsi_dcs_write_seq(dsi, 0xff, 0x10);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
-	dsi_dcs_write_seq(dsi, 0xb0, 0x00);
-	dsi_dcs_write_seq(dsi, 0xff, 0xe0);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
-	dsi_dcs_write_seq(dsi, 0x35, 0x82);
-	dsi_dcs_write_seq(dsi, 0xff, 0xf0);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
-	dsi_dcs_write_seq(dsi, 0x5a, 0x00);
-	dsi_dcs_write_seq(dsi, 0x9c, 0x00);
-	dsi_dcs_write_seq(dsi, 0xbe, 0x08);
-	dsi_dcs_write_seq(dsi, 0xff, 0xc0);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
-	dsi_dcs_write_seq(dsi, 0x9c, 0x11);
-	dsi_dcs_write_seq(dsi, 0x9d, 0x11);
-	dsi_dcs_write_seq(dsi, 0xff, 0x10);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
 
-	ret = mipi_dsi_dcs_set_display_brightness(dsi, 0x00c1);
-	if (ret < 0) {
-		dev_err(dev, "Failed to set display brightness: %d\n", ret);
-		return ret;
-	}
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x20);
+	mipi_dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0xee, 0x60);
+	mipi_dsi_dcs_write_seq(dsi, 0xef, 0x06);
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x23);
+	mipi_dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0x00, 0x80);
+	mipi_dsi_dcs_write_seq(dsi, 0x01, 0x84);
+	mipi_dsi_dcs_write_seq(dsi, 0x05, 0x2d);
+	mipi_dsi_dcs_write_seq(dsi, 0x06, 0x00);
+	mipi_dsi_dcs_write_seq(dsi, 0x07, 0x00);
+	mipi_dsi_dcs_write_seq(dsi, 0x08, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0x09, 0x45);
+	mipi_dsi_dcs_write_seq(dsi, 0x11, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0x12, 0x95);
+	mipi_dsi_dcs_write_seq(dsi, 0x15, 0x68);
+	mipi_dsi_dcs_write_seq(dsi, 0x16, 0x0b);
+	mipi_dsi_dcs_write_seq(dsi, 0x29, 0x0a);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_SET_PARTIAL_ROWS, 0xff);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_SET_PARTIAL_COLUMNS, 0xfe);
+	mipi_dsi_dcs_write_seq(dsi, 0x32, 0xfd);
+	mipi_dsi_dcs_write_seq(dsi, 0x33, 0xfb);
+	mipi_dsi_dcs_write_seq(dsi, 0x34, 0xf8);
+	mipi_dsi_dcs_write_seq(dsi, 0x35, 0xf5);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_SET_ADDRESS_MODE, 0xf3);
+	mipi_dsi_dcs_write_seq(dsi, 0x37, 0xf2);
+	mipi_dsi_dcs_write_seq(dsi, 0x38, 0xf2);
+	mipi_dsi_dcs_write_seq(dsi, 0x39, 0xf2);
 
-	dsi_dcs_write_seq(dsi, MIPI_DCS_WRITE_CONTROL_DISPLAY, 0x2c);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_WRITE_POWER_SAVE, 0x00);
-	dsi_dcs_write_seq(dsi, 0xff, 0x23);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
-	dsi_dcs_write_seq(dsi, 0x10, 0x82);
-	dsi_dcs_write_seq(dsi, 0x11, 0x01);
-	dsi_dcs_write_seq(dsi, 0x12, 0x95);
-	dsi_dcs_write_seq(dsi, 0x15, 0x68);
-	dsi_dcs_write_seq(dsi, 0x16, 0x0b);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_SET_PARTIAL_ROWS, 0xff);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_SET_PARTIAL_COLUMNS, 0xfd);
-	dsi_dcs_write_seq(dsi, 0x32, 0xfb);
-	dsi_dcs_write_seq(dsi, 0x33, 0xfa);
-	dsi_dcs_write_seq(dsi, 0x34, 0xf9);
-	dsi_dcs_write_seq(dsi, 0x35, 0xf7);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_SET_ADDRESS_MODE, 0xf5);
-	dsi_dcs_write_seq(dsi, 0x37, 0xf4);
-	dsi_dcs_write_seq(dsi, 0x38, 0xf1);
-	dsi_dcs_write_seq(dsi, 0x39, 0xef);
-
-	ret = mipi_dsi_dcs_set_pixel_format(dsi, 0xed);
+	ret = mipi_dsi_dcs_set_pixel_format(dsi, 0xef);
 	if (ret < 0) {
 		dev_err(dev, "Failed to set pixel format: %d\n", ret);
 		return ret;
 	}
 
-	dsi_dcs_write_seq(dsi, 0x3b, 0xeb);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_SET_3D_CONTROL, 0xea);
-	dsi_dcs_write_seq(dsi, 0x3f, 0xe8);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_SET_VSYNC_TIMING, 0xe6);
-	dsi_dcs_write_seq(dsi, 0x41, 0xe5);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_GET_SCANLINE, 0xff);
-	dsi_dcs_write_seq(dsi, 0x46, 0xf3);
-	dsi_dcs_write_seq(dsi, 0x47, 0xe8);
-	dsi_dcs_write_seq(dsi, 0x48, 0xdd);
-	dsi_dcs_write_seq(dsi, 0x49, 0xd3);
-	dsi_dcs_write_seq(dsi, 0x4a, 0xc9);
-	dsi_dcs_write_seq(dsi, 0x4b, 0xbe);
-	dsi_dcs_write_seq(dsi, 0x4c, 0xb3);
-	dsi_dcs_write_seq(dsi, 0x4d, 0xa9);
-	dsi_dcs_write_seq(dsi, 0x4e, 0x9f);
-	dsi_dcs_write_seq(dsi, 0x4f, 0x95);
-	dsi_dcs_write_seq(dsi, 0x50, 0x8b);
+	mipi_dsi_dcs_write_seq(dsi, 0x3b, 0xec);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_SET_3D_CONTROL, 0xe9);
+	mipi_dsi_dcs_write_seq(dsi, 0x3f, 0xe5);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_SET_VSYNC_TIMING, 0xe5);
+	mipi_dsi_dcs_write_seq(dsi, 0x41, 0xe5);
+	mipi_dsi_dcs_write_seq(dsi, 0x2a, 0x13);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_GET_SCANLINE, 0xff);
+	mipi_dsi_dcs_write_seq(dsi, 0x46, 0xf4);
+	mipi_dsi_dcs_write_seq(dsi, 0x47, 0xe7);
+	mipi_dsi_dcs_write_seq(dsi, 0x48, 0xda);
+	mipi_dsi_dcs_write_seq(dsi, 0x49, 0xcd);
+	mipi_dsi_dcs_write_seq(dsi, 0x4a, 0xc0);
+	mipi_dsi_dcs_write_seq(dsi, 0x4b, 0xb3);
+	mipi_dsi_dcs_write_seq(dsi, 0x4c, 0xb2);
+	mipi_dsi_dcs_write_seq(dsi, 0x4d, 0xb2);
+	mipi_dsi_dcs_write_seq(dsi, 0x4e, 0xb2);
+	mipi_dsi_dcs_write_seq(dsi, 0x4f, 0x99);
+	mipi_dsi_dcs_write_seq(dsi, 0x50, 0x80);
 
-	ret = mipi_dsi_dcs_set_display_brightness(dsi, 0x0081);
+	ret = mipi_dsi_dcs_set_display_brightness(dsi, 0x0068);
 	if (ret < 0) {
 		dev_err(dev, "Failed to set display brightness: %d\n", ret);
 		return ret;
 	}
 
-	dsi_dcs_write_seq(dsi, 0x52, 0x77);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_WRITE_CONTROL_DISPLAY, 0x6d);
-	dsi_dcs_write_seq(dsi, 0x54, 0x65);
-	dsi_dcs_write_seq(dsi, 0x58, 0xff);
-	dsi_dcs_write_seq(dsi, 0x59, 0xf8);
-	dsi_dcs_write_seq(dsi, 0x5a, 0xf3);
-	dsi_dcs_write_seq(dsi, 0x5b, 0xee);
-	dsi_dcs_write_seq(dsi, 0x5c, 0xe9);
-	dsi_dcs_write_seq(dsi, 0x5d, 0xe4);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_SET_CABC_MIN_BRIGHTNESS, 0xdf);
-	dsi_dcs_write_seq(dsi, 0x5f, 0xda);
-	dsi_dcs_write_seq(dsi, 0x60, 0xd4);
-	dsi_dcs_write_seq(dsi, 0x61, 0xcf);
-	dsi_dcs_write_seq(dsi, 0x62, 0xca);
-	dsi_dcs_write_seq(dsi, 0x63, 0xc5);
-	dsi_dcs_write_seq(dsi, 0x64, 0xc0);
-	dsi_dcs_write_seq(dsi, 0x65, 0xbb);
-	dsi_dcs_write_seq(dsi, 0x66, 0xb6);
-	dsi_dcs_write_seq(dsi, 0x67, 0xb1);
-	dsi_dcs_write_seq(dsi, 0xa0, 0x11);
-	dsi_dcs_write_seq(dsi, 0xff, 0x27);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
-	dsi_dcs_write_seq(dsi, MIPI_DCS_SET_VSYNC_TIMING, 0x25);
-	dsi_dcs_write_seq(dsi, 0xff, 0x10);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0x52, 0x66);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_WRITE_CONTROL_DISPLAY, 0x66);
+	mipi_dsi_dcs_write_seq(dsi, 0x54, 0x66);
+	mipi_dsi_dcs_write_seq(dsi, 0x2b, 0x0e);
+	mipi_dsi_dcs_write_seq(dsi, 0x58, 0xff);
+	mipi_dsi_dcs_write_seq(dsi, 0x59, 0xfb);
+	mipi_dsi_dcs_write_seq(dsi, 0x5a, 0xf7);
+	mipi_dsi_dcs_write_seq(dsi, 0x5b, 0xf3);
+	mipi_dsi_dcs_write_seq(dsi, 0x5c, 0xef);
+	mipi_dsi_dcs_write_seq(dsi, 0x5d, 0xe3);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_SET_CABC_MIN_BRIGHTNESS, 0xda);
+	mipi_dsi_dcs_write_seq(dsi, 0x5f, 0xd8);
+	mipi_dsi_dcs_write_seq(dsi, 0x60, 0xd8);
+	mipi_dsi_dcs_write_seq(dsi, 0x61, 0xd8);
+	mipi_dsi_dcs_write_seq(dsi, 0x62, 0xcb);
+	mipi_dsi_dcs_write_seq(dsi, 0x63, 0xbf);
+	mipi_dsi_dcs_write_seq(dsi, 0x64, 0xb3);
+	mipi_dsi_dcs_write_seq(dsi, 0x65, 0xb2);
+	mipi_dsi_dcs_write_seq(dsi, 0x66, 0xb2);
+	mipi_dsi_dcs_write_seq(dsi, 0x67, 0xb2);
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x20);
+	mipi_dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0x1f, 0xc0);
+	mipi_dsi_dcs_write_seq(dsi, 0x20, 0xc0);
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x25);
+	mipi_dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0xd6, 0x80);
+	mipi_dsi_dcs_write_seq(dsi, 0xd7, 0x02);
+	mipi_dsi_dcs_write_seq(dsi, 0xdd, 0x02);
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x27);
+	mipi_dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_SET_VSYNC_TIMING, 0x20);
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x10);
+	mipi_dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0xc0, 0x03);
 
-	ret = mipi_dsi_dcs_exit_sleep_mode(dsi);
+	ret = mipi_dsi_dcs_set_display_brightness(dsi, 0xb50d);
 	if (ret < 0) {
-		dev_err(dev, "Failed to exit sleep mode: %d\n", ret);
+		dev_err(dev, "Failed to set display brightness: %d\n", ret);
 		return ret;
 	}
+
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_WRITE_CONTROL_DISPLAY, 0x24);
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x10);
+	mipi_dsi_dcs_write_seq(dsi, 0x11, 0x00);
+
 	msleep(70);
 
 	ret = mipi_dsi_dcs_set_display_on(dsi);
@@ -165,117 +164,115 @@ static int nt36672c_on(struct nt36672c *ctx)
 		dev_err(dev, "Failed to set display on: %d\n", ret);
 		return ret;
 	}
-	msleep(40);
+	
+	msleep(40);	
 
-	dsi_dcs_write_seq(dsi, 0xff, 0x27);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
-	dsi_dcs_write_seq(dsi, 0x3f, 0x01);
-	dsi_dcs_write_seq(dsi, 0x43, 0x08);
+	mipi_dsi_dcs_write_seq(dsi, 0x29, 0x00);
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x27);
+	mipi_dsi_dcs_write_seq(dsi, 0xfb, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0x3f, 0x01);
+	mipi_dsi_dcs_write_seq(dsi, 0x43, 0x08);
+	mipi_dsi_dcs_write_seq(dsi, MIPI_DCS_SET_VSYNC_TIMING, 0x25);
+	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x10);
 
 	return 0;
 }
 
-static int nt36672c_off(struct nt36672c *ctx)
+static int j20s_42_02_0b_dsc_off(struct j20s_42_02_0b_dsc *ctx)
 {
 	struct mipi_dsi_device *dsi = ctx->dsi;
-	struct device *dev = &dsi->dev;
-	int ret;
 
-	dsi_dcs_write_seq(dsi, 0xff, 0x10);
-	dsi_dcs_write_seq(dsi, 0xfb, 0x01);
-
-	ret = mipi_dsi_dcs_set_display_off(dsi);
-	if (ret < 0) {
-		dev_err(dev, "Failed to set display off: %d\n", ret);
-		return ret;
-	}
-	usleep_range(16000, 17000);
-
-	ret = mipi_dsi_dcs_enter_sleep_mode(dsi);
-	if (ret < 0) {
-		dev_err(dev, "Failed to enter sleep mode: %d\n", ret);
-		return ret;
-	}
-	msleep(60);
+	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
 
 	return 0;
 }
 
-static int nt36672c_prepare(struct drm_panel *panel)
+static int j20s_42_02_0b_dsc_prepare(struct drm_panel *panel)
 {
-	struct nt36672c *ctx = to_nt36672c(panel);
+	struct j20s_42_02_0b_dsc *ctx = to_j20s_42_02_0b_dsc(panel);
 	struct device *dev = &ctx->dsi->dev;
 	struct drm_dsc_picture_parameter_set pps;
 	int ret;
-
+	
 	if (ctx->prepared)
 		return 0;
 
-	nt36672c_reset(ctx);
+	ret = regulator_enable(ctx->supply);
+	if (ret < 0) {
+		dev_err(dev, "Failed to enable regulator: %d\n", ret);
+		return ret;
+	}
 
-	ret = nt36672c_on(ctx);
+	j20s_42_02_0b_dsc_reset(ctx);
+
+	ret = j20s_42_02_0b_dsc_on(ctx);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
 		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
 		return ret;
 	}
 
-	if (ctx->dsi->dsc) {
-		drm_dsc_pps_payload_pack(&pps, ctx->dsi->dsc);
-		print_hex_dump(KERN_DEBUG, "DSC params:", DUMP_PREFIX_NONE,
-				16, 1, &pps, sizeof(pps), false);
+	drm_dsc_pps_payload_pack(&pps, &ctx->dsc);
 
-		ret = mipi_dsi_picture_parameter_set(ctx->dsi, &pps);
-		if (ret < 0) {
-			dev_err(panel->dev, "failed to set pps: %d\n", ret);
-			return ret;
-		}
-		msleep(28);
+	ret = mipi_dsi_picture_parameter_set(ctx->dsi, &pps);
+	if (ret < 0) {
+		dev_err(panel->dev, "failed to transmit PPS: %d\n", ret);
+		regulator_disable(ctx->supply);
+		return ret;
 	}
 
+	ret = mipi_dsi_compression_mode(ctx->dsi, true);
+	if (ret < 0) {
+		dev_err(dev, "failed to enable compression mode: %d\n", ret);
+		return ret;
+	}
+
+	msleep(28); /* TODO: Is this panel-dependent? */
+	
 	ctx->prepared = true;
 	return 0;
 }
 
-static int nt36672c_unprepare(struct drm_panel *panel)
+static int j20s_42_02_0b_dsc_unprepare(struct drm_panel *panel)
 {
-	struct nt36672c *ctx = to_nt36672c(panel);
+	struct j20s_42_02_0b_dsc *ctx = to_j20s_42_02_0b_dsc(panel);
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
-
+	
 	if (!ctx->prepared)
 		return 0;
 
-	ret = nt36672c_off(ctx);
+	ret = j20s_42_02_0b_dsc_off(ctx);
 	if (ret < 0)
 		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
 
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+	regulator_disable(ctx->supply);
 
 	ctx->prepared = false;
 	return 0;
 }
 
-static const struct drm_display_mode nt36672c_mode = {
-	.clock = (1080 + 73 + 12 + 40) * (2400 + 32 + 2 + 30) * 120 / 1000,
+static const struct drm_display_mode j20s_42_02_0b_dsc_mode = {
+	.clock = (1080 + 67 + 12 + 40) * (2400 + 33 + 2 + 30) * 120 / 1000,
 	.hdisplay = 1080,
-	.hsync_start = 1080 + 73,
-	.hsync_end = 1080 + 73 + 12,
-	.htotal = 1080 + 73 + 12 + 40,
+	.hsync_start = 1080 + 67,
+	.hsync_end = 1080 + 67 + 12,
+	.htotal = 1080 + 67 + 12 + 40,
 	.vdisplay = 2400,
-	.vsync_start = 2400 + 32,
-	.vsync_end = 2400 + 32 + 2,
-	.vtotal = 2400 + 32 + 2 + 30,
-	.width_mm = 70,
-	.height_mm = 154,
+	.vsync_start = 2400 + 33,
+	.vsync_end = 2400 + 33 + 2,
+	.vtotal = 2400 + 33 + 2 + 30,
+	.width_mm = 695,
+	.height_mm = 1546,
 };
 
-static int nt36672c_get_modes(struct drm_panel *panel,
-					  struct drm_connector *connector)
+static int j20s_42_02_0b_dsc_get_modes(struct drm_panel *panel,
+				       struct drm_connector *connector)
 {
 	struct drm_display_mode *mode;
 
-	mode = drm_mode_duplicate(connector->dev, &nt36672c_mode);
+	mode = drm_mode_duplicate(connector->dev, &j20s_42_02_0b_dsc_mode);
 	if (!mode)
 		return -ENOMEM;
 
@@ -289,31 +286,26 @@ static int nt36672c_get_modes(struct drm_panel *panel,
 	return 1;
 }
 
-static const struct drm_panel_funcs nt36672c_panel_funcs = {
-	.prepare = nt36672c_prepare,
-	.unprepare = nt36672c_unprepare,
-	.get_modes = nt36672c_get_modes,
+static const struct drm_panel_funcs j20s_42_02_0b_dsc_panel_funcs = {
+	.prepare = j20s_42_02_0b_dsc_prepare,
+	.unprepare = j20s_42_02_0b_dsc_unprepare,
+	.get_modes = j20s_42_02_0b_dsc_get_modes,
 };
 
-static int nt36672c_probe(struct mipi_dsi_device *dsi)
+static int j20s_42_02_0b_dsc_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
-	struct nt36672c *ctx;
-	struct drm_dsc_config *dsc;
+	struct j20s_42_02_0b_dsc *ctx;
 	int ret;
 
 	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
 		return -ENOMEM;
-
-	ctx->supplies[0].supply = "vddio";
-	ctx->supplies[1].supply = "vci";
-	ctx->supplies[2].supply = "vcie";
-	ret = devm_regulator_bulk_get(dev, ARRAY_SIZE(ctx->supplies),
-				      ctx->supplies);
-	if (ret < 0)
-		return dev_err_probe(dev, ret, "Failed to get regulators\n");
-
+	
+	ctx->supply = devm_regulator_get(dev, "vddio");
+	if (IS_ERR(ctx->supply))
+		return dev_err_probe(dev, PTR_ERR(ctx->supply),
+				     "Failed to get vddio regulator\n");
 
 	ctx->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(ctx->reset_gpio))
@@ -328,10 +320,9 @@ static int nt36672c_probe(struct mipi_dsi_device *dsi)
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_CLOCK_NON_CONTINUOUS |
 			  MIPI_DSI_MODE_LPM;
 
-	ctx->panel.prepare_prev_first = true;
-
-	drm_panel_init(&ctx->panel, dev, &nt36672c_panel_funcs,
+	drm_panel_init(&ctx->panel, dev, &j20s_42_02_0b_dsc_panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
+	ctx->panel.prepare_prev_first = true;
 
 	ret = drm_panel_of_backlight(&ctx->panel);
 	if (ret)
@@ -339,20 +330,24 @@ static int nt36672c_probe(struct mipi_dsi_device *dsi)
 
 	drm_panel_add(&ctx->panel);
 
-	/* The panel is DSC panel only, set the dsc params */
-	dsi->dsc = dsc = devm_kzalloc(&dsi->dev, sizeof(*dsc), GFP_KERNEL);
-	if (!dsc)
-		return -ENOMEM;
+	/* This panel only supports DSC; unconditionally enable it */
+	dsi->dsc = &ctx->dsc;
 
-	dsc->dsc_version_major = 0x1;
-	dsc->dsc_version_minor = 0x1;
+	ctx->dsc.dsc_version_major = 1;
+	ctx->dsc.dsc_version_minor = 1;
 
-	dsc->slice_height = 20;
-	dsc->slice_width = 540;
-	dsc->slice_count = 2;
-	dsc->bits_per_component = 8;
-	dsc->bits_per_pixel = 8 << 4; /* 4 fractional bits */
-	dsc->block_pred_enable = true;
+	/* TODO: Pass slice_per_pkt = 2 */
+	ctx->dsc.slice_height = 20;
+	ctx->dsc.slice_width = 540;
+	/*
+	 * TODO: hdisplay should be read from the selected mode once
+	 * it is passed back to drm_panel (in prepare?)
+	 */
+	WARN_ON(1080 % ctx->dsc.slice_width);
+	ctx->dsc.slice_count = 1080 / ctx->dsc.slice_width;
+	ctx->dsc.bits_per_component = 8;
+	ctx->dsc.bits_per_pixel = 8 << 4; /* 4 fractional bits */
+	ctx->dsc.block_pred_enable = true;
 
 	ret = mipi_dsi_attach(dsi);
 	if (ret < 0) {
@@ -364,9 +359,9 @@ static int nt36672c_probe(struct mipi_dsi_device *dsi)
 	return 0;
 }
 
-static void nt36672c_remove(struct mipi_dsi_device *dsi)
+static void j20s_42_02_0b_dsc_remove(struct mipi_dsi_device *dsi)
 {
-	struct nt36672c *ctx = mipi_dsi_get_drvdata(dsi);
+	struct j20s_42_02_0b_dsc *ctx = mipi_dsi_get_drvdata(dsi);
 	int ret;
 
 	ret = mipi_dsi_detach(dsi);
@@ -376,22 +371,22 @@ static void nt36672c_remove(struct mipi_dsi_device *dsi)
 	drm_panel_remove(&ctx->panel);
 }
 
-static const struct of_device_id nt36672c_of_match[] = {
-	{ .compatible = "huaxing,nt36672c" },
+static const struct of_device_id j20s_42_02_0b_dsc_of_match[] = {
+	{ .compatible = "huaxing,nt36672c" }, // FIXME
 	{ /* sentinel */ }
 };
-MODULE_DEVICE_TABLE(of, nt36672c_of_match);
+MODULE_DEVICE_TABLE(of, j20s_42_02_0b_dsc_of_match);
 
-static struct mipi_dsi_driver nt36672c_driver = {
-	.probe = nt36672c_probe,
-	.remove = nt36672c_remove,
+static struct mipi_dsi_driver j20s_42_02_0b_dsc_driver = {
+	.probe = j20s_42_02_0b_dsc_probe,
+	.remove = j20s_42_02_0b_dsc_remove,
 	.driver = {
-		.name = "panel-huaxing-nt36672c",
-		.of_match_table = nt36672c_of_match,
+		.name = "panel-j20s-42-02-0b-dsc",
+		.of_match_table = j20s_42_02_0b_dsc_of_match,
 	},
 };
-module_mipi_dsi_driver(nt36672c_driver);
+module_mipi_dsi_driver(j20s_42_02_0b_dsc_driver);
 
-MODULE_AUTHOR("Arseniy Velikanov <adomerlee@gmail.com>");
-MODULE_DESCRIPTION("DRM driver for nt36672c huaxing fhd video mode dsi panel");
-MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("linux-mdss-dsi-panel-driver-generator <fix@me>"); // FIXME
+MODULE_DESCRIPTION("DRM driver for xiaomi 42 02 0b video mode dsc dsi panel");
+MODULE_LICENSE("GPL");
